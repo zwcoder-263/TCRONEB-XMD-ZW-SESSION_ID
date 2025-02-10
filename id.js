5966123c91bd8c3ebee9b7211bea441d59977283
LiL CRONE <zwcoder263@gmail.com>
2025-02-10 11:40:19 -0800

    Update id.js

--- id.js
+++ id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "Lazack-wer123ghbkjnhbf";
+  let characters = "Tcroneb-wer123ghbkjnhbf";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
